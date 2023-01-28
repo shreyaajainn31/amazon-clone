7fb20191fadeddd1920385e69a0bc53f23a6a472
shreyajain <shreyajain981@gmail.com>
2023-01-28 13:57:22 -0800

    initial navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from './components/Navbar';
+import './components/Navbar.css';
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <Navbar/>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+import 'material-icons/iconfont/material-icons.css';
 
 function Navbar(){
 
     return (
         <div>
             <nav className = 'Navbar'>
-            <ul>
-                <li NavLink exact to = "/"></li>
-            </ul>
+            <div className= 'amazon-logo'> <img src = "/amazon.png" alt = 'amazon-logo'/>  </div>
+            <div className = 'items'>
+                <div className = 'search-container'>
+                <input type = "text" placeholder="search"></input>
+                <li><i className="material-icons" id = "search">search</i></li>
+                </div>
+                <li>Login</li>
+                <li>Return & Orders</li>
+                <li> Amazon Prime</li>
+                <li><i className="material-icons" id = "basket">shopping_basket</i></li>
+            </div>
             </nav>
         </div>
     );
