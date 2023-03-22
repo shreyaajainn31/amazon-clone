63abda3e435d9f164d73911231df70e261d47bbe
shreyajain <shreyajain981@gmail.com>
2023-03-22 12:05:32 -0700

    initial home page ready!

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,33 +1,52 @@
-import React, {useState} from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './Navbar.css';
-import 'material-icons/iconfont/material-icons.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import "./Navbar.css";
+import "material-icons/iconfont/material-icons.css";
 import Logo from "./images/amazon.png";
-import {useNavigate, Link} from "react-router-dom";
 
-function Navbar(){
-    const navigate = useNavigate();
+function Navbar() {
+  const [numberOfItems, setNumberOfItems] = useState(0);
 
-    const [numberOfItems, setNumberOfItems] = useState(0);
-
-
-    return (
-        <div>
-            <nav className = 'Navbar'>
-            <div className = 'items'>
-                <div className = 'search-container'>
-                <input type = "text" placeholder="search"></input>
-                <li><i className="material-icons" id = "search">search</i></li>
-                </div>
-                <li><Link to ="/login">Login</Link></li>
-                <li><Link to ="/return">Return & Orders</Link></li>
-                <li><Link to = "/prime"> Amazon Prime </Link></li>
-                <li><i className="material-icons" id = "basket"><Link to ="/cart">shopping_basket</Link></i> {numberOfItems}</li>
-            </div>
-            </nav>
+  return (
+    <div>
+      <nav className="Navbar">
+        <div className="logo-container">
+          <Link to="/">
+            <img className="amazon-logo" src={Logo} alt="Amazon Logo" />
+          </Link>
         </div>
-    );
-
+        <div className="items">
+          <div className="search-container">
+            <input type="text" placeholder="search" />
+            <li>
+              <i className="material-icons" id="search">
+                search
+              </i>
+            </li>
+          </div>
+          <div className="nav-links">
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/return">Return & Orders</Link>
+            </li>
+            <li>
+              <Link to="/prime">Amazon Prime</Link>
+            </li>
+            <li>
+              <Link to="/cart">
+                <i className="material-icons" id="basket">
+                  shopping_basket
+                </i>{" "}
+                {numberOfItems}
+              </Link>
+            </li>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
