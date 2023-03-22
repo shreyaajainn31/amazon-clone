49ba9d6f04c8fdc917a0a20e2a8f07e202b2cfa9
shreyajain <shreyajain981@gmail.com>
2023-03-22 11:49:12 -0700

    had some changes

--- src/components/Navbar/Pages/Home.js
+++ src/components/Navbar/Pages/Home.js
@@ -1,25 +1,23 @@
 import React from "react";
 import Navbar from "../Navbar";
+// src/components/images/home.jpg
 import Image from '../images/home.jpg';
 import Product from "../../Product/Product";
 import './Pages.css';
+function Home(){
 
-function Home() {
-  return (
-    <div>
-      <Navbar />
-      <div className="image-container">
-        <img src={Image} className="img" />
-        <p className="home-text">
-          You are on amazon.com. You can also go to Amazon India for millions of products with fast local delivery.{" "}
-          <a href="https://www.amazon.in/" target="_blank">
-            Click here to go to amazon.in
-          </a>
-        </p>
-      </div>
-      <Product />
-    </div>
-  );
+    return (
+        <div>
+            <Navbar/>
+            <div className = "image-container">
+            <img src = {Image} className = 'img'/>
+            <p className = "home-text">You are on amazon.com. You can also go to Amazon India for million of products with fast local delivery. <a href= "https://www.amazon.in/" target="_blank"> Click here to go to amazon.in</a></p>
+            </div>
+            <Product/>
+
+           
+        </div>
+    );
+
 }
-
 export default Home;
